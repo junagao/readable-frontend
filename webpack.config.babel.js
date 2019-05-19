23971365eb7896d8ff6b4b2af9c235f21c5a4025
Juliane Nagao <julianens@gmail.com>
2019-05-19 12:59:07 +0200

    refactor: webpack config imports

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, 'src', 'index.html'),
@@ -15,7 +15,7 @@ const miniCssExtractPlugin = new MiniCssExtractPlugin({
 
 const cleanWebpackPlugin = new CleanWebpackPlugin();
 
-module.exports = {
+export default {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'build'),
