b4c57d5803ce7eee49c33f4314e6d9e426e4aaab
Juliane Nagao <julianens@gmail.com>
2019-06-14 12:30:04 +0200

    feat: add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => (
-  <div>Header</div>
+  <div className="menu-container">
+    <div className="menu-left">
+      <Link to="/" className="logo">Readable</Link>
+    </div>
+    <div className="menu-right">
+      <Link to="/posts/new" className="create-post">add new post</Link>
+      <GoogleAuth />
+    </div>
+  </div>
 );
 
 export default Header;
