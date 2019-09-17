4f350a75458a70c49799773e7261c3b04e3a1f28
Juliane Nagao <julianens@gmail.com>
2019-09-17 10:31:48 +0200

    refactor: CommentList CommentItem

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,44 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
+import CommentItem from './CommentItem';
 
 const CommentList = ({
   comments,
   commentCount,
-  author,
   currentUserName,
 }) => (
   <div>
     {commentCount > 0 && comments.map(comment => (
       <div className="comment-content" key={comment.id}>
-        <p>{comment.body}</p>
-        <p className="comment-details">
-          <span className="comment-author">{`by ${comment.author} `}</span>
-          <span className="comment-date">
-            {moment(comment.timestamp)
-              .startOf('minute')
-              .fromNow()}
-          </span>
-          {author === currentUserName && (
-          <React.Fragment>
-            <span className="comment-details-separator">|</span>
-            <span>
-              <Link to={`/comments/edit/${comment.id}`} className="edit-bt" type="button">
-                edit
-              </Link>
-              <span className="comment-details-separator">|</span>
-              <Link
-                to={`/comments/delete/${comment.id}`}
-                className="delete-bt"
-                type="button"
-              >
-                delete
-              </Link>
-            </span>
-          </React.Fragment>
-          )}
-        </p>
+        <CommentItem
+          {...comment}
+          currentUserName={currentUserName}
+        />
       </div>
     ))}
   </div>
@@ -47,7 +22,6 @@ const CommentList = ({
 CommentList.propTypes = {
   comments: PropTypes.instanceOf(Object).isRequired,
   commentCount: PropTypes.number.isRequired,
-  author: PropTypes.string.isRequired,
   currentUserName: PropTypes.string,
 };
 
